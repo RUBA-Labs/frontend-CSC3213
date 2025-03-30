e348d38886475184148e27606caf57d5fd19e481
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-03-30 19:53:30 +0530

    Dark mode basic
    Add a dark mode switcher

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import ThemeSwitch from './components/ThemeSwitch'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl ">Resource Management System</h1>
+      <ThemeSwitch/>
+      <h1 className="text-4xl mt-2">Resource Management System</h1>
       <p className="text-lg mt-4 mx-5 text-justify"> The Resource Management System (RMS) is a web-based platform designed to streamline resource allocation and scheduling within the Arts Faculty. It provides an intuitive interface for managing lab allocations, creating and organizing timetables, and handling exam payment claims for lecturers. The system ensures efficient coordination between students, lecturers, and administrators, improving accessibility and reducing administrative workload.</p>
+
     </main>
    
   );
