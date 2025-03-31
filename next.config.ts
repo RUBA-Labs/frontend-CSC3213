086cd175754e0ed6720be4d05f428fa73148570d
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-03-31 18:28:25 +0530

    Add fix

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/frontend-CSC3213" : "",
-  assetPrefix: isProd ? "/frontend-CSC3213/" : "",
+  /* config options here */
 };
 
 export default nextConfig;
