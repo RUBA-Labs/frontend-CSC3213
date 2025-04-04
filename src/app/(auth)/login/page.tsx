ab3f07bd279d4a8fe4d485a8a0d499c93d04deca
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-04-04 19:15:44 +0530

    Password test

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,20 @@
+'use client'
 import LoginForm from '../components/loginForm'
+
 
 export default function login() {
 
+    const handleLogin = (userData: { email: string; password: string }) => {
+        console.log("User Email:", userData.email);
+        console.log("User Password:", userData.password);
+        // You can send this data to an API or process it further
+    };
+
     return (
-        <div className="flex justify-evenly items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
+        <div className="flex justify-evenly items-center min-h-screen bg-gradient-to-br from-fuchsia-950 to-purple-950">
             <h1 className="text-4xl font-bold text-white mb-4">Welcome to Our App</h1>
 
-            <LoginForm/>
+            <LoginForm onLogin={handleLogin}/>
         </div>
     )
 
