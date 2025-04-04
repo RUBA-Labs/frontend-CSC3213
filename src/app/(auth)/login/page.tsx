d08f03977cfdc9acffa2990d07dd7bdf7edec705
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-04-04 16:17:27 +0530

    Login update
    Login update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,18 +1,12 @@
+import LoginForm from '../components/loginForm'
+
 export default function login() {
 
     return (
-        <div className="bg-cyan-950 text-align-center flex flex-col justify-center items-center h-screen ">
+        <div className="flex justify-evenly items-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600">
             <h1 className="text-4xl font-bold text-white mb-4">Welcome to Our App</h1>
-            login page
-            <div className="max-w-sm rounded-2xl overflow-hidden shadow-lg bg-white p-4 hover:shadow-2xl transition-shadow border border-gray-300">
-                <div className="p-4 border border-gray-400 rounded-xl bg-white bg-opacity-50">
-                    <h2 className="text-xl font-semibold mb-2">Card Title</h2>
-                    <p className="text-gray-600">This is a brief description of the card. It provides useful information.</p>
-                    <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-all">
-                        Learn More
-                    </button>
-                </div>
-            </div>
+
+            <LoginForm/>
         </div>
     )
 
