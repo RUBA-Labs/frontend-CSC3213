c155cd8ee4246ab4f2b1958a425435b4f8df73e5
RAVINDU WEERAKOON <w.m.t.ravindusampath@gmail.com>
2025-04-10 12:21:21 +0530

    Fix and restacher

--- src/app/(auth)/components/button.tsx
+++ src/app/(auth)/components/button.tsx
@@ -1,3 +1,5 @@
+import "../style/button.css";
+
 export default function Button({ type, label }: {
     type: "button" | "submit" | "reset" | undefined,
     label: string
@@ -5,10 +7,7 @@ export default function Button({ type, label }: {
     return (
         <button
             type={type}
-            className="w-full
-            bg-dark5 text-dark3 py-2 rounded-lg hover:bg-dark3 hover:text-dark5 transition duration-300 
-            dark:bg-dark3 dark:text-dark5 py-2 rounded-lg dark:hover:bg-dark5 dark:hover:text-dark3 transition duration-300"
-        >
+            className="button">
             {label}
         </button>
     );
