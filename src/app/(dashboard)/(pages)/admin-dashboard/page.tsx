339c87262c7c9fbe3c27dbd4278146f2aa846d38
Uditha Wijethunga <udithalakshan321@gmail.com>
2025-08-08 12:38:51 +0530

    Add dashboard pages and components with styles
    Introduces individual dashboard pages and corresponding components for academic, admin, developer, exam claim admin, first year admin, lab allocation admin, non-academic, student, time table admin, and user dashboards. Also adds shared CSS styles for page layout and buttons to support consistent UI across dashboards.

---
+++ src/app/(dashboard)/(pages)/admin-dashboard/page.tsx
@@ -0,0 +1,22 @@
+
+import Footer from "@/components/footer";
+import React from "react";
+import "../../style/page.css";
+import AdminDashboard from "../../components/adminDashboard";
+
+export default function AdminDashboardPage() {
+    return (
+        <div className="pagebackground min-h-screen flex flex-col">
+            {/* Middle - Main Content */}
+            <div className="pageflexbox flex flex-1 justify-center items-start">
+                <div className="w-full flex justify-center">
+                    <div className="scale-90 sm:scale-95 lg:scale-100 w-full">
+                        <AdminDashboard />
+                    </div>
+                </div>
+            </div>
+            {/* Bottom - Footer */}
+            <Footer />
+        </div>
+    );
+}
